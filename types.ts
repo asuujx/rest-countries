ad0ff3f7da54622c84de69a56cf5e294282e5044
assuy <43821373+assuy@users.noreply.github.com>
2023-07-21 17:03:06 +0200

    Update types.ts country interface

--- types.ts
+++ types.ts
@@ -1,9 +1,15 @@
+
 export interface Country {
-   name: {
-      common: string;
-   }
-   capital: string;
-   population: number;
-   region: string;
-   flags: string[];
+  name: {
+    common: string;
+  };
+  population: number;
+  region: string;
+  subregion: string;
+  capital: string;
+  tld: string[];
+  currencies: string[];
+  languages: string[];
+  borders: string[];
+  flags: string[];
 };
